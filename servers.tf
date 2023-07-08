e382b0e8ce2802ac46a8255c97d8721aa3d0de04
ThumburuAditya <chandramouliadityat@gmail.com>
2023-07-08 19:01:14 +0530

    ec2 creation code

---
+++ servers.tf
@@ -0,0 +1,33 @@
+data "aws_ami" "centos" {
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+  owners           = ["973714476881"]
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "frontend"
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "mongodb"
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "catalogue"
+  }
+}
+

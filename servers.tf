85392de48d14740628953842c8fc0b34eb99ed20
ThumburuAditya <chandramouliadityat@gmail.com>
2023-07-08 19:11:40 +0530

    Route 53 creation

--- servers.tf
+++ servers.tf
@@ -1,10 +1,10 @@
 data "aws_ami" "centos" {
+  owners           = ["973714476881"]
   most_recent      = true
   name_regex       = "Centos-8-DevOps-Practice"
-  owners           = ["973714476881"]
 }
 
-resource "aws_instance" "web" {
+resource "aws_instance" "frontend" {
   ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
 
@@ -13,7 +13,15 @@ resource "aws_instance" "web" {
   }
 }
 
-resource "aws_instance" "web" {
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z06720662E9995DMBFZQQ"
+  name    = "frontend-dev.thumburuaditya.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.frontend.private_ip]
+}
+
+resource "aws_instance" "mongodb" {
   ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
 
@@ -22,7 +30,15 @@ resource "aws_instance" "web" {
   }
 }
 
-resource "aws_instance" "web" {
+resource "aws_route53_record" "mongodb" {
+  zone_id = "Z06720662E9995DMBFZQQ"
+  name    = "mongodb-dev.thumburuaditya.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mongodb.private_ip]
+}
+
+resource "aws_instance" "catalogue" {
   ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
 
@@ -31,3 +47,10 @@ resource "aws_instance" "web" {
   }
 }
 
+resource "aws_route53_record" "catalogue" {
+  zone_id = "Z06720662E9995DMBFZQQ"
+  name    = "catalogue-dev.thumburuaditya.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.catalogue.private_ip]
+}

9a652cf95baa93ffd5d61400d30a524571ed93d4
ThumburuAditya <chandramouliadityat@gmail.com>
2023-07-09 16:58:45 +0530

    adding the provisioner

--- servers.tf
+++ servers.tf
@@ -9,6 +9,25 @@ resource "aws_instance" "instance" {
   }
 }
 
+resource "null_resource" "provisioner" {
+  depends_on = [aws_instance.instance,aws_route53_record.records]
+  for_each = var.components
+  provisioner "remote-exec" {
+    connection {
+      type     = "ssh"
+      user     = "centos"
+      password = "DevOps321"
+      host     = aws_instance.instance[each.value["name"]].private_ip
+    }
+    inline = [
+      "rm -rf roboshop-shell",
+      "git clone",
+      "cd roboshop-shell",
+      "sudo bash ${each.value["name"]}.sh"
+    ]
+  }
+  }
+
 resource "aws_route53_record" "records" {
   for_each = var.components
   zone_id = "Z06720662E9995DMBFZQQ"

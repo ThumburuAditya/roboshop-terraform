b94bed6a534aa29fd5711e4f144f775fc430ad9b
ThumburuAditya <chandramouliadityat@gmail.com>
2023-07-12 00:29:22 +0530

    modifying the code for ansible

---
+++ module/main.tf
@@ -0,0 +1,37 @@
+resource "aws_instance" "instance" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
+
+  tags = {
+    Name = var.component_name
+  }
+}
+
+resource "null_resource" "provisioner" {
+  count = var.provisioner ? 1 : 0
+  depends_on = [aws_instance.instance,aws_route53_record.records]
+  provisioner "remote-exec" {
+    connection {
+      type     = "ssh"
+      user     = "centos"
+      password = "DevOps321"
+      host     = aws_instance.instance.private_ip
+    }
+    inline = [
+      "rm -rf roboshop-shell",
+      "git clone",
+      "cd roboshop-shell",
+      "sudo bash ${var.component_name}.sh ${var.password}"
+    ]
+  }
+}
+
+resource "aws_route53_record" "records" {
+  for_each = var.components
+  zone_id = "Z06720662E9995DMBFZQQ"
+  name    = "${each.value["name"]}-dev.thumburuaditya.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.instance[each.value["name"]].private_ip]
+}
---
+++ module/vars.tf
@@ -0,0 +1,8 @@
+variable "components" {}
+variable "env" {}
+variable "instance_type" {}
+variable "component_name" {}
+variable "password" {}
+variable "provisioner" {
+  default = false
+}

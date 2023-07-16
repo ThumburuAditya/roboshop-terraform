a8beee9f1e48adcbfb7e229997010e5353eea5e7
ThumburuAditya <chandramouliadityat@gmail.com>
2023-07-17 02:35:38 +0530

    writing script

--- module/main.tf
+++ module/main.tf
@@ -29,3 +29,56 @@ resource "aws_route53_record" "records" {
   ttl     = 30
   records = [aws_instance.instance .private_ip]
 }
+
+resource "aws_iam_role" "role" {
+  name = "${var.component_name}- ${var.env}-role"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
+
+resource "aws_iam_role_policy" "ssm_ps_policy" {
+  name = "${var.component_name}- ${var.env}-role-ssm-ps-policy"
+  role = aws_iam_role.role.id
+  policy = jsonencode(
+    {
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Sid": "VisualEditor0",
+          "Effect": "Allow",
+          "Action": [
+            "ssm:GetParameterHistory",
+            "ssm:GetParametersByPath",
+            "ssm:GetParameters",
+            "ssm:GetParameter"
+          ],
+          "Resource": "arn:aws:ssm:us-east-1:363684552706:parameter/${var.env}.${var.component_name}.*"
+        },
+        {
+          "Sid": "VisualEditor1",
+          "Effect": "Allow",
+          "Action": "ssm:DescribeParameters",
+          "Resource": "*"
+        }
+      ]
+    }
+  )
+}

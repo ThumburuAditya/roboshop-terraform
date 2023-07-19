d1f7d42980790c3d1ca147250a6c5057af6d527e
ThumburuAditya <chandramouliadityat@gmail.com>
2023-07-19 23:52:11 +0530

    correcting the script

--- module/locals.tf
+++ module/locals.tf
@@ -10,6 +10,6 @@ locals{
 
   app_commands = [
     "sudo labauto ansible",
-    "ansible-pull -i localhost, -U https://github.com/ThumburuAditya/roboshop-ansible roboshop.yml -e env=${var.env} -e role_name=${var.component_name}"
+    "ansible-pull -i localhost, -U https://github.com/ThumburuAditya/roboshop-ansible.git roboshop.yml -e env=${var.env} -e role_name=${var.component_name}"
   ]
 }

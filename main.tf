e9d16397c63a735762f53bdcb80c97cfee0a2197
ThumburuAditya <chandramouliadityat@gmail.com>
2023-08-09 23:10:51 +0530

    Adding the code

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source = "git::https://https://github.com/ThumburuAditya/tf-module-vpc.git"
+  source = "git::https://github.com/ThumburuAditya/tf-module-vpc.git"
 
   for_each = var.vpc
   cidr_block = each.value["cidr_block"]

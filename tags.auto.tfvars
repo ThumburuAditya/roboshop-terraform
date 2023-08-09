c901f5d6d22ff20d32ed917515615682b7a6afd4
ThumburuAditya <chandramouliadityat@gmail.com>
2023-08-09 22:56:22 +0530

    Adding the code

--- tags.auto.tfvars
+++
@@ -1,7 +0,0 @@
-tags = {
-  business_unit ="ecommerce"
-  business_type ="retail"
-  env = var.env
-  project ="roboshop"
-  cost_center = 100
-}
---
+++ tags.tf
@@ -0,0 +1,9 @@
+locals {
+  tags = {
+    business_unit = "ecommerce"
+    business_type = "retail"
+    env           = var.env
+    project       = "roboshop"
+    cost_center   = 100
+  }
+}

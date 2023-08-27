3a0c3c9591d8c47ae51631ad2ce05fd80ddbac3e
ThumburuAditya <chandramouliadityat@gmail.com>
2023-08-27 22:47:12 +0530

    Adding the code

--- main.tf
+++ main.tf
@@ -11,5 +11,5 @@ module "app" {
   source = "git::https://github.com/ThumburuAditya/tf-module-app.git"
   for_each = var.app
   instance_type = each.value["instance_type"]
-  subnet_id = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
+  subnet_ids = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
 }

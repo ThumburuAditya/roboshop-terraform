f4805b9696afbc6daa23fbae9e4ff153ec2b5dcf
ThumburuAditya <chandramouliadityat@gmail.com>
2023-08-27 21:11:25 +0530

    Adding the code

--- main.tf
+++ main.tf
@@ -7,6 +7,9 @@ module "vpc" {
   env = var.env
 }
 
-output "vpc" {
-  value = module.vpc
+module "web" {
+  source = "git::https://github.com/ThumburuAditya/tf-module-app.git"
+  for_each = var.app
+  instance_type = each.value["instance_type"]
+  subnet_id = element(lookup(lookup(lookup(module.vpc,"main", null ),each.value["subnet_name"], null ),"subnet_ids", null ), 0)
 }

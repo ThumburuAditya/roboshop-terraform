fdb57039f2300acb6b9c328da3d325251817b51e
ThumburuAditya <chandramouliadityat@gmail.com>
2023-09-06 01:44:10 +0530

    Adding the code

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ module "docdb" {
   for_each = var.docdb
   subnets = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
   allow_db_cidr =lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_db_cidr"], null), "subnet_cidrs", null)
-  engine_version = each.value["enginer_version"]
+  engine_version = each.value["engine_version"]
   instance_count = each.value["instance_count"]
   instance_class = each.value["instance_class"]
 

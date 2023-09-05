571ce840907b843d068f6300dbff3e82b463eb6b
ThumburuAditya <chandramouliadityat@gmail.com>
2023-09-06 01:05:52 +0530

    Adding the code

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ module "docdb" {
   source = "git::https://github.com/ThumburuAditya/tf-module-docdb.git"
   for_each = var.docdb
   subnet_ids = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
-  allow_db_cird =lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_db_cidr"], null), "subnet_cidrs", null)
+  allow_db_cidr =lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_db_cidr"], null), "subnet_cidrs", null)
   engine_version = each.value["enginer_version"]
 
   tags = local.tags
